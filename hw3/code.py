aa6162bbfbcfecabd175e73e87e29c08f46ac077
Riley Bruins <ribru17@hotmail.com>
2023-04-25 19:31:34 -0700

    hw3: up to 3

---
+++ hw3/code.py
@@ -0,0 +1,22 @@
+from functools import reduce
+
+
+def convert_to_decimal(bits):
+    exponents = range(len(bits)-1, -1, -1)
+    nums = [bit * (2 ** exp) for bit, exp in zip(bits, exponents)]
+    return reduce(lambda acc, num: acc + num, nums)
+
+
+def parse_csv(lines):
+    return [(y[0], int(y[1])) for y in [x.split(',') for x in lines]]
+
+
+def unique_characters(sentence):
+    return {c for c in sentence}
+
+
+def squares_dict(lower_bound, upper_bound):
+    return {x: x**2 for x in range(lower_bound, upper_bound+1)}
+
+
+print(squares_dict(1, 5))
